42623d7e254cb89a3ce157a67bda9c6fdf87eebb
Cdlm1138 <cristian.lucumi00@usc.edu.co>
2018-12-13 10:26:00 -0500

    update YelpRequestSOAP function

---
+++ app/platforms/browser/www/js/YelpReviewSOAP.js
@@ -0,0 +1,31 @@
+
+	  function YelpRequestSOAP(YelpID,Callback) {
+                var wsUrl = "https://backservices.forkstation.com/Services/Gateway.php?action=getYelpReviews&data-id="+YelpID;
+				
+launchSOAP(wsUrl,Callback);    
+		}
+
+
+
+        function processError(request, status, error) {
+			if (window.location.href.indexOf("-es")!=-1){
+            alertcreate("Error Interno",request.responseText + " " + status,"Aceptar");}
+			else{
+			alertcreate("Internal Error",request.responseText + " " + status,"Accept");}
+			}
+          
+
+function launchSOAP(SOAPUrl,Callback){
+	//$.get(SOAPUrl, "jsonp",  function(data) {
+   //OKFunction(data);
+//});
+$.ajax({
+   type: 'GET',
+    url: SOAPUrl,
+    contentType: "text; charset=\"utf-8\"",
+    success: function(json) {
+		Callback(json);
+    },
+    error: processError
+});
+}
